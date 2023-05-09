e148a30bf05abfc893e7d000360a598b906cf3e1
Genti13 <alanaumente@gmail.com>
2023-05-09 16:00:01 -0300

    Terminado ej 16

---
+++ php/ej_14.php
@@ -0,0 +1,3 @@
+<?php
+include_once('../html/ej_14.html');
+?>
---
+++ php/ej_14_imprimirMenu.php
@@ -0,0 +1,7 @@
+<?php
+
+foreach ($comidas_elegidas as $element) {
+    echo "<h4 class='text-center'>" . "$element" . "</h4>";
+}
+
+?>
